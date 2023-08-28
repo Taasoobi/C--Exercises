d88d750ffddde91d342a2e35862f414fbbbbf3cf
ZeNoobi <ctaaseen@gmail.com>
2023-08-28 00:28:56 -0700

    Moved maxNumber Function Outside

--- test.c
+++ test.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    int numberOfItems;
-    int* arr;//just a pointer
-    int inputNmbr;
-printf("Enter the number of items: ");
-scanf("%d", &numberOfItems);
 
-//enter memory into array
-arr= (int*)malloc(numberOfItems * sizeof(int));
-
-if(arr == NULL){
-    printf("\n memory allocation failed");
-} else {
-    printf("\n memory allocated successfully!");
-    printf("\n Here is size: %d", numberOfItems);
-}
-
-for (int i = 0; i < numberOfItems; i++)
-{
-    printf("\n Enter number into array at index %d: ", i);
-    scanf("%d", &inputNmbr);
-    arr[i]=inputNmbr;
-    printf("%d", arr[i]);
-}
-
+//function to check max number, returns the max value;
 int maxNumber(int arr[], int size){
     int max = arr[0];
     for (int i = 0; i < size; i++)
@@ -32,19 +9,50 @@ int maxNumber(int arr[], int size){
         if (arr[i] > max)
         {
             max = arr[i];
-        }
-        
+        }   
     }
-    
     return max;
 }
 
 
+int main(){
+int numberOfItems;
+int* arr;//just a pointer
+int inputNmbr;
 int ans;
+
+
+//prompt user
+printf("Enter the number of items: ");
+scanf("%d", &numberOfItems);
+
+
+//enter memory into array
+arr= (int*)malloc(numberOfItems * sizeof(int));
+
+
+//memory allocation check
+if(arr == NULL){
+    printf("\n memory allocation failed");
+} else {
+    printf("\n memory allocated successfully!");
+    printf("\n Here is size: %d", numberOfItems);
+}
+
+
+//enter value for each indices
+for (int i = 0; i < numberOfItems; i++)
+{
+    printf("\n Enter number into array at index %d: ", i);
+    scanf("%d", &inputNmbr);
+    arr[i]=inputNmbr;
+    //printf("%d", arr[i]);
+}
+
+//call function
 ans = maxNumber(arr, numberOfItems);
-
+//print answer
 printf("\n The max number is: %d", ans);
-
 
 free(arr);
 return 0;
