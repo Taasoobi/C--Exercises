46ad25c41259601b088b631dc867d2d55d89fb23
ZeNoobi <ctaaseen@gmail.com>
2023-08-28 00:15:02 -0700

    Working Version

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 int main(){
     int numberOfItems;
     int* arr;//just a pointer
+    int inputNmbr;
 printf("Enter the number of items: ");
 scanf("%d", &numberOfItems);
 
@@ -14,23 +15,35 @@ if(arr == NULL){
 } else {
     printf("\n memory allocated successfully!");
     printf("\n Here is size: %d", numberOfItems);
-    //printf("\n Here is arr: ");
-    //printf(arr);
 }
 
-/*
-for(int i=0; i<numberOfItems-1; i++){
-    arr[i]=3;
-    printf("item in arr[%d] ", i);
-    printf("is: ");
-    //printf(arr[i]);
-}
-*/
 for (int i = 0; i < numberOfItems; i++)
 {
-    arr[i]=3;
+    printf("\n Enter number into array at index %d: ", i);
+    scanf("%d", &inputNmbr);
+    arr[i]=inputNmbr;
     printf("%d", arr[i]);
 }
+
+int maxNumber(int arr[], int size){
+    int max = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        
+    }
+    
+    return max;
+}
+
+
+int ans;
+ans = maxNumber(arr, numberOfItems);
+
+printf("\n The max number is: %d", ans);
 
 
 free(arr);
